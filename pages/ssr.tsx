4e78b583b7109259da94eaf0ae1632e69a80f7a9
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-31 01:21:17 +0900

    add ssr sample

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,5 +1,33 @@
-function SSR() {
-  return <h1>Hello Server Side Rendering</h1>;
+import Link from 'next/link';
+import { fetchAll } from '../utils/qiitaApi';
+import { Item } from '../interfaces';
+
+type Props = {
+  items: Item[];
+};
+
+function SSR({ items }: Props) {
+  return (
+    <div>
+      <h1>Hello Server Side Rendering</h1>
+      {items.map(item => (
+        <p key={item.id}>
+          <Link href={item.url}>
+            <a target="_blank">{item.title}</a>
+          </Link>
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const items = await fetchAll();
+  return {
+    props: {
+      items,
+    },
+  };
 }
 
 export default SSR;
