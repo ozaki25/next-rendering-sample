a6249b1da6cdfb318f7b4cf79bebf0c0d765b5fe
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-31 01:20:39 +0900

    add log to fetch qiita api

--- utils/qiitaApi.ts
+++ utils/qiitaApi.ts
@@ -4,6 +4,7 @@ import { Item } from '../interfaces';
 const url = 'https://qiita.com/api/v2/users/ozaki25/items';
 
 export async function fetchAll(): Promise<Item[]> {
+  console.log(`fetch ${url}`);
   const res = await fetch(url);
   const json = await res.json();
   return json;
