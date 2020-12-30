8246f22af9953ee467610175ea1447cfc07b56d2
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-31 00:53:09 +0900

    add csr sample

--- pages/csr.tsx
+++ pages/csr.tsx
@@ -1,11 +1,27 @@
 import { useEffect, useState } from 'react';
-import fetch from 'node-fetch';
+import Link from 'next/link';
+import { fetchAll } from '../utils/qiitaApi';
+import { Item } from '../interfaces';
 
 function CSR() {
-  const [items, setItems] = useState([]);
-  useEffect(() => {}, []);
+  const [items, setItems] = useState<Item[]>([]);
+  useEffect(() => {
+    fetchAll().then(result => setItems(result));
+  }, []);
 
-  return <h1>Hello Client Side Rendering</h1>;
+  return (
+    <div>
+      <h1>Hello Client Side Rendering</h1>
+      {!items.length && <p>...loading</p>}
+      {items.map(item => (
+        <p key={item.id}>
+          <Link href={item.url}>
+            <a target="_blank">{item.title}</a>
+          </Link>
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default CSR;
