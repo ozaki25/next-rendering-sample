0c7af06720bd358085c06471aa31a52949169acc
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-31 01:25:32 +0900

    add ssg sample

---
+++ pages/ssg.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { fetchAll } from '../utils/qiitaApi';
+import { Item } from '../interfaces';
+
+type Props = {
+  items: Item[];
+};
+
+function SSG({ items }: Props) {
+  return (
+    <div>
+      <h1>Hello Server Side Rendering</h1>
+      {items.map(item => (
+        <p key={item.id}>
+          <Link href={item.url}>
+            <a target="_blank">{item.title}</a>
+          </Link>
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const items = await fetchAll();
+  return {
+    props: {
+      items,
+    },
+  };
+}
+
+export default SSG;
